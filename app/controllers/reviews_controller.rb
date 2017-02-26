4e37df728b101fe75a7a3400a720770e684a2dcc
Josiah Berkebile <praenato14@gmail.com>
2017-02-26 03:03:26 -0600

    make reviews a json api

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  clear_respond_to
+  respond_to :json
 
   # GET /reviews
   # GET /reviews.json
@@ -26,28 +28,20 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    if @review.save
+      render :show, status: :created, location: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    if @review.update(review_params)
+      render :show, status: :ok, location: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
     end
   end
 
@@ -55,16 +49,15 @@ class ReviewsController < ApplicationController
   # DELETE /reviews/1.json
   def destroy
     @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    format.json { head :no_content }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: e.message.to_json, status: :not_found
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
