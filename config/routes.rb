ffe4caf251bb4e8a3c796d88cbf33d6287223559
Jake LaCombe <jake.lacombe2@gmail.com>
2017-04-02 20:14:46 -0500

    Redirect all non api routes to the react app

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
     resources :opportunities, except: :new, defaults: { formats: :json }
     devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   end
+
+  get "/*path" => 'pages#index', via: [:get]
+
   root 'pages#index'
 end
