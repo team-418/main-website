3aa6fcf956c2b5d3db2e93896916e8d9d98d1d7b
Josiah Berkebile <praenato14@gmail.com>
2017-06-10 12:57:07 -0500

    user sign-out tests complete (w/ errors)

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -3,3 +3,28 @@ Given "that the user has successfully loaded the website" do
   @csrf_token = Nokogiri::HTML(page.html).css('meta[name="csrf-token"]').attr('content').value
 end
 
+Given(/^the user is signed in$/) do
+  @user = create(:user)
+
+  @res = post(
+    '/api/users/sign_in.json',
+    {
+      user: {
+        email: @user.email,
+        password: @user.password
+      }
+    },
+    {
+      'X-CSRF-Token': @csrf_token,
+     'X-Transaction': "Create New User Session"
+    }
+  )
+  @parsed_res_body = JSON.parse!(@res.body)
+end
+
+Given "the user is NOT signed in" do
+  # Create the user, but don't sign in
+  @user = create(:user)
+end
+
+
---
+++ features/step_definitions/user_signout_steps.rb
@@ -0,0 +1,30 @@
+When(/^the user tries to sign\-out$/) do
+  @res = delete(
+    'api/users/sign_out.json',
+    { 'X-Transaction': 'Destroy Current User Session' }
+  )
+end
+
+When "the user tries to sign-out twice in a row" do
+   @res = delete(
+    'api/users/sign_out.json',
+    { 'X-Transaction': 'Destroy Current User Session' }
+  )
+
+  @res = delete(
+    'api/users/sign_out.json',
+    { 'X-Transaction': 'Destroy Current User Session' }
+  )
+end
+
+Then "the server should indicate it destroyed the current user's session via a 204: No Content status" do
+  expect(@res.status).to be(204)
+end
+
+Then(/^the json response should indicate the user was signed\-out$/) do
+  expect(@parsed_res_body).to eq({ "success" => true })
+end
+
+Then(/^the json response should indicate there was nothnig to do since the user wasn't signed-in$/) do
+  expect(@parsed_res_body).to eq(nil)
+end
